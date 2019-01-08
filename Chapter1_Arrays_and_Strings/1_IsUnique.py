7e6713d45c0f9c583d1dc1301f2cf51a588bd838
Yuezhou <lujc515@163.com>
2019-01-08 16:56:44 +0900

    1.1 Is Unique

---
+++ Chapter1_Arrays_and_Strings/1_IsUnique.py
@@ -0,0 +1,66 @@
+class Solution:
+    def is_unique(self, input_string):
+        # Assuming character set is ASCII (128 characters)
+        if len(input_string) > 128:
+            return False
+        
+        char_set = [False for _ in range(128)]
+        for char in input_string:
+            unicode_point = ord(char)
+            if char_set[unicode_point]:
+                # Char already found in string
+                return False
+            char_set[unicode_point] = True
+        return True
+
+
+    def is_unique_2(self, input_string):
+        input_string = sorted(input_string)
+        if len(input_string) <= 1:
+            return True
+        for index in range(0, len(input_string) - 1): # No need to check the last character
+            if input_string[index] == input_string[index + 1]:
+                return False
+        return True
+
+    def is_unique_3(self, input_string):
+        if len(input_string) != len(set(input_string)): # Use set()
+            return False
+        return True
+
+
+if __name__ == "__main__":
+    S = Solution()
+    tests = ["abcd", "", "hb 627j=()", "testing", "23ds2", "hb 627jh=j ()"]
+    expected = [True, True, True, False, False, False]
+    total = len(tests)
+
+    passed = 0
+    for index in range(0, len(tests)):
+        output = S.is_unique(tests[index])
+        if output == expected[index]:
+            passed += 1
+        else:
+            print("Test #{} Failed! INPUT: {}; OUTPUT: {}; EXPECTED: {}".format(
+                index + 1, tests[index], output, expected[index]))
+    print("{} of {} tests passed".format(passed, total))
+
+    passed = 0
+    for index in range(0, len(tests)):
+        output = S.is_unique_2(tests[index])
+        if output == expected[index]:
+            passed += 1
+        else:
+            print("Test #{} Failed! INPUT: {}; OUTPUT: {}; EXPECTED: {}".format(
+                index + 1, tests[index], output, expected[index]))
+    print("{} of {} tests passed for alternative method 2".format(passed, total))
+
+    passed = 0
+    for index in range(0, len(tests)):
+        output = S.is_unique_3(tests[index])
+        if output == expected[index]:
+            passed += 1
+        else:
+            print("Test #{} Failed! INPUT: {}; OUTPUT: {}; EXPECTED: {}".format(
+                index + 1, tests[index], output, expected[index]))
+    print("{} of {} tests passed for alternative method 3".format(passed, total))
